24eecc966186ddbd856a855aa55a309171c0aa39
İNTİKAM21 CYBER <167871296+Intikam21kurucu@users.noreply.github.com>
2024-05-10 07:36:49 +0300

    Update

---
+++ intconsole.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+const { exec } = require('child_process');
+
+exec('python3 intconsoleV2.py', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Hata: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.error(`Stderr: ${stderr}`);
+    return;
+  }
+  console.log(`Stdout: ${stdout}`);
+});
