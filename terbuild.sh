84feb02c623452c590b4a851779b437dc9f39213
İNTİKAM21 CYBER <167871296+Intikam21kurucu@users.noreply.github.com>
2024-05-15 19:03:03 +0300

    Update terbuild.sh

--- terbuild.sh
+++ terbuild.sh
@@ -2,11 +2,17 @@
 
 # Bu script, Termux'ta 'intconsole' komutu için bir alias oluşturur ve .bashrc dosyasına ekler.
 
+# İlk çalıştırma için bir kontrol dosyası oluşturun.
+if [ ! -f "$HOME/intframework/.install_done" ]; then
+    python3 "$HOME/intframework/installintconsole.py"
+    touch "$HOME/intframework/.install_done"
+fi
+
 # 'intconsole' komutunu tanımlayın.
-echo "alias intconsole='cd intframework && python3 installintconsole && python3 intconsoleV2.py'" >> ~/.bashrc
+echo "alias intconsole='cd ~/intframework && python3 intconsoleV2.py && source ~/.bashrc'" >> ~/.bashrc
 
 # .bashrc dosyasını yeniden yükleyin.
 source ~/.bashrc
 
-# Scriptin çalıştığını doğrulayın.
-echo "downloaded intconsole and added it to the intconsole command line"
+# Kullanıcıya scriptin başarıyla eklendiğini bildirin.
+echo "intconsole komutu '.bashrc' dosyanıza başarıyla eklendi ve hazır."
