f9d7b11f5b30aabe70b873c2a5cecd852421f967
İNTİKAM21 CYBER <167871296+Intikam21kurucu@users.noreply.github.com>
2024-05-22 19:37:14 +0300

    Update intai.py

--- intai.py
+++ intai.py
@@ -106,6 +106,3 @@ def main():
         if user_input:  # Sadece kullanıcı bir şey yazdığında yanıt ver
             response = provide_info(user_input, language)
             print(response)
-
-if __name__ == "__main__":
-    main()
