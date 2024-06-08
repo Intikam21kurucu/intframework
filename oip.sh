7a8780533b904537716b27afbb7ef3506b1c0ddc
İNTİKAM21 CYBER <167871296+Intikam21kurucu@users.noreply.github.com>
2024-06-08 17:16:16 +0300

    Update oip.sh
    Signed-off-by: İNTİKAM21 CYBER <167871296+Intikam21kurucu@users.noreply.github.com>

--- oip.sh
+++ oip.sh
@@ -1,6 +1,7 @@
+GNU nano 8.0                       oip.sh
 chmod +x oip
-mkdir -p ~/bin
-mv oip ~/bin/
-echo 'export PATH=$HOME/bin:$PATH' >> ~/.profile
-source ~/.profile
+mkdir -p $HOME/intframework
+mv oip $HOME/intframework/
+echo 'export PATH=$HOME/intframework:$PATH' >> $HOME/.bashrc
+source $HOME/.bashrc
 oip -h
