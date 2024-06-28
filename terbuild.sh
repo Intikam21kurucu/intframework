6293358dba49cd7cc7a8760a93546407110a8d8b
İNTİKAM21 CYBER <167871296+Intikam21kurucu@users.noreply.github.com>
2024-06-28 15:39:09 +0300

    Update terbuild.sh

--- terbuild.sh
+++ terbuild.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 # Bu script, Termux'ta 'intconsole' komutu için bir alias oluşturur ve .bashrc dosyasına ekler.
@@ -11,20 +12,19 @@ if [ ! -f "$HOME/intframework/.install_done" ]; then
 fi
 
 # 'intconsole' komutunu tanımlayın.
-echo "alias intconsole='cd ~/intframework && python3 intconsoleV2.py && source ~/.bashrc'" >> ~/.bashrc
+echo "alias intconsole='cd ~/intframework && python3 intconsoleV3.py && source ~/.bashrc'" >> ~/.bashrc
+
+echo "alias intweb='python3 /data/data/com.termux/files/home/intframework/intweb'" >> ~/.bashrc
+
+echo "alias introjan='python3 /data/data/com.termux/files/home/intframework/introjan'" >> ~/.bashrc
+
+echo "alias intvenom='python3 /data/data/com.termux/files/home/intframework/intvenom.py" >> ~/.bashrc
+
+echo "alias intofficial='python3 /data/data/com.termux/files/home/intframework/İNTOFFİCİAL.py" >> ~/.bashrc
 
 # .bashrc dosyasını yeniden yükleyin.
 source ~/.bashrc
 
-touch ~/.bash_aliases
-nano ~/.bash_aliases
-chmod +x introjan
-mkdir -p ~/bin/
-mv introjan ~/bin/
-echo $PATH
-export PATH=$HOME/bin:$PATH
-alias introjan='python3 introjan'>>~/.bash_aliases
-source ~/.bash_aliases
 introjan -h
 
 chmod +x oip
@@ -33,5 +33,3 @@ mv oip ~/intmodules/
 echo 'export PATH=$HOME/intmodules:$PATH' >> ~/.profile
 source ~/.profile
 oip -h
-
-echo "LAUNCEDED INTCONSOLE"
