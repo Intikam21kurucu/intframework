5df07e16ad1aeca6290e0a30a2b346921f2bce59
İNTİKAM21 CYBER <167871296+Intikam21kurucu@users.noreply.github.com>
2024-07-24 17:03:52 +0300

    Delete wrapper.js
    Signed-off-by: İNTİKAM21 CYBER <167871296+Intikam21kurucu@users.noreply.github.com>

--- wrapper.js
+++
@@ -1,9 +0,0 @@
-const { exec } = require('child_process');
-exec('python intconsoleV2.py', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-  console.error(`stderr: ${stderr}`);
-});
