50aebc656c6b431177aec5de1c03be9a0c925c63
İNTİKAM21 CYBER <167871296+Intikam21kurucu@users.noreply.github.com>
2024-07-25 10:53:38 +0300

    Update terbuild.sh

--- terbuild.sh
+++ terbuild.sh
@@ -1,36 +1,106 @@
-#!/bin/bash
+#!/usr/bin/env/python3
 
-# Bu script, Termux'ta 'intconsole' komutu için bir alias oluşturur ve .bashrc dosyasına ekler.
 
-chmod +x terbuild.sh
+import os
+import sys
+import time
 
-# İlk çalıştırma için bir kontrol dosyası oluşturun.
-if [ ! -f "$HOME/intframework/.install_done" ]; then
-    python3 "$HOME/intframework/installintconsole.py"
-    touch "$HOME/intframework/.install_done"
-fi
 
-# 'intconsole' komutunu tanımlayın.
-echo "alias intconsole='cd ~/intframework && python3 intconsoleV4.py && source ~/.bashrc'" >> ~/.bashrc
+def p():
+	input("please enter to continue")
 
-echo "alias intweb='python3 /data/data/com.termux/files/home/intframework/intweb'" >> ~/.bashrc
+def loading(text):
+    for i in range(10):
+        sys.stdout.write("\r" + "[+] " + text + "." * i)
+        time.sleep(0.1)
+        sys.stdout.flush()
+    print("\033[92m" + "\r" + "[+] " + text + "....................OK!\033[0m")
 
-echo "alias introjan='python3 /data/data/com.termux/files/home/intframework/introjan'" >> ~/.bashrc
+def prompt():
+    input("Press ENTER to continue. ")
 
-echo "alias intvenom='python3 /data/data/com.termux/files/home/intframework/intvenom.py'" >> ~/.bashrc
+def start_exploit():
+    loading("Accessing Backdoor.........")
+    loading("Crafting İntconsole...........")
+    loading("Sending İntconsole............")
 
-echo "alias intcam='python3 /data/data/com.termux/files/home/intframework/intcam.py'" >> ~/.bashrc
+user_input = input("Do you using termux?[Y/n]")
 
-echo "alias intofficial='python3 /data/data/com.termux/files/home/intframework/İNTOFFİCİAL.py'" >> ~/.bashrc
+if user_input == "y" or "Y":
+	start_exploit()	
+	loading("İnstalling İntconsole...........")
+	os.system("apt-get install python3")
+	os.system("apt-get install python3-pip")
+	os.system("pip3 install --upgrade")
+	os.system("pip3 install pyfiglet")
+	os.system("pip3 install colorama")
+	os.system("pip3 install Fore")
+	os.system("pip3 install init")
+	os.system("pip3 install threading")
+	os.system("pip3 install requests")
+	os.system("pip3 install time")
+	os.system("pip3 install sys")
+	os.system("pip3 install os")
+	os.system("pip3 install base64")
+	os.system("pip3 install time")
+	os.system("pip3 install argparse")	
+	os.system("pkg install bcrypt")
+	os.system("pkg install python-bcrypt")
+	os.system("pkg install python-cryptography")
+	os.system("pip install paramiko")
+	os.system("pkg install nmap")
 
-# .bashrc dosyasını yeniden yükleyin.
-source ~/.bashrc
 
-introjan -h
+	loading("İntconsole Downloading..........")
+	print("""
+	installed intconsole 
+	using = intconsole 
+	else : please command using=='run intconsole'		
+	""")
+	k = input("Do you command is working[Y/n]")
+	
+	
+	if k == "y" or k == "Y":
+		print("please continue ")
+		p()
+	if k == "n" or "N":
+		print("please continue")
+		p()
+	
 
-chmod +x oip
-mkdir -p ~/intmodules
-mv oip ~/intmodules/
-echo 'export PATH=$HOME/intmodules:$PATH' >> ~/.profile
-source ~/.profile
-oip -h
+if user_input == "n" or input == "N":
+	start_exploit()	
+	loading("İnstalling İntconsole...........")
+	os.system("sudo apt-get install python3")
+	os.system("sudo apt-get install python3-pip")
+	os.system("sudo pip3 install --upgrade")
+	os.system("sudo apt install python3-pyfiglet")
+	os.system("sudo apt install python3-colorama")
+	os.system("sudo apt install python3-Fore")
+	os.system("sudo apt install python3-init")
+	os.system("sudo install python3-threading")
+	os.system("sudo apt install requests")
+	os.system("sudo apt install python3-time")
+	os.system("sudo apt install python3-sys")
+	os.system("sudo apt install python3-os")
+	os.system("sudo apt install python3-base64")
+	os.system("sudo apt install python3-time")
+	os.system("sudo apt install python3-argparse")
+	loading("İntconsole Downloading..........")
+	print("""
+	installed intconsole 
+	using = intconsole 
+	else : please command using=='run intconsole'		
+	""")
+	k = input("Do you command is working[Y/n]")
+	
+	
+	if k == "n" or k == "N":
+	   	os.system("cd ~")
+	   	os.system("nano .bashrc")
+	   	os.system("source ~/.bashrc")
+	   	os.system("echo \"alias intconsole='python3 /usr/bin/intframework/intconsoleV4.py'\" >> ~/.bashrc")
+	   	p()
+	elif k == "y" or k == "Y":
+		print("type intconsole to start please")
+		p()	
