2579dd8d47b1a3db280346aed752d3da30d22a52
İNTİKAM21 CYBER <167871296+Intikam21kurucu@users.noreply.github.com>
2024-08-11 12:17:47 +0300

    Update intconsole.js

--- intconsole.js
+++ intconsole.js
@@ -2,7 +2,7 @@
 
 const { exec } = require('child_process');
 
-exec('python3 intconsoleV2.py', (error, stdout, stderr) => {
+exec('python3 intconsoleV4.py', (error, stdout, stderr) => {
   if (error) {
     console.error(`Hata: ${error.message}`);
     return;
