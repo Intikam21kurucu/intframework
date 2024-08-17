7d4524d17f55ca7cb0c7f6cbcf3c064cc8660de6
İNTİKAM21 CYBER <167871296+Intikam21kurucu@users.noreply.github.com>
2024-08-17 16:03:16 +0300

    Update terbuild.sh

--- terbuild.sh
+++ terbuild.sh
@@ -12,17 +12,36 @@ fi
 
 cd ~
 
+mv intconsole $PREFIX/bin
+
 pkg install zsh
 
 # 'intconsole' komutunu tanımlayın.
-echo "alias intconsole='cd ~/intframework && python3 intconsoleV4.py && source ~/.bashrc'" >> ~/.bashrc
-echo "alias intweb='python3 /data/data/com.termux/files/home/intframework/modules/intweb'" >> ~/.bashrc
-echo "alias introjan='python3 /data/data/com.termux/files/home/intframework/modules/introjan'" >> ~/.bashrc
-echo "alias intvenom='python3 /data/data/com.termux/files/home/intframework/modules/intvenom.py'" >> ~/.bashrc
-echo "alias intcam='python3 /data/data/com.termux/files/home/intframework/modules/intcam.py'" >> ~/.bashrc
-echo "alias intninja='python3 /data/data/com.termux/files/home/intframework/modules/intninja.py'" >> ~/.bashrc
-echo "alias intmail='python3 /data/data/com.termux/files/home/intframework/modules/intmail.py'" >> ~/.bashrc
-echo "alias intmeterpreter='python3 /data/data/com.termux/files/home/intframework/modules/intmeterpreter.py'" >> ~/.bashrc
+
+echo "alias ll=ls -la" >> ~/.bashrc or ~/.zshrc
+echo "alias intupdate='apt update && apt upgrade -y && rm -rf ~/intframework && git clone https://github.com/Intikam21kurucu/intframework'" >> ~/.bashrc or ~/.zshrc
+echo "export INTFRAMEWORK_PATH=$PREFIX/opt/intframework" >> ~/.bashrc or ~/.zshrc
+echo "export INTDIR=$PREFIX/opt/" >> ~/.bashrc or ~/.zshrc
+echo "alias intframework=$INTFRAMEWORK_PATH"
+echo "alias c='clear'" >> ~/.bashrc or ~/.zshrc
+alias gs='git status' >> ~/.bashrc or ~/.zshrc
+
+echo 'intconsole() { ORIGINAL_DIR=$(pwd); cd $INTDIR; python3 intconsoleV4.py; cd $ORIGINAL_DIR; [ -n "$BASH" ] && source ~/.bashrc || source ~/.zshrc; }; alias intconsole="intconsole"' >> ~/.bashrc && echo 'intconsole() { ORIGINAL_DIR=$(pwd); cd $INTDIR; python3 intconsoleV4.py; cd $ORIGINAL_DIR; [ -n "$BASH" ] && source ~/.bashrc || source ~/.zshrc; }; alias intconsole="intconsole"' >> ~/.zshrc
+
+echo "alias introjan='python3 $INTFRAMEWORK_PATH/introjan'" >> ~/.bashrc or ~/.zshrc
+
+echo "alias intvenom='python3 $INTFRAMEWORK_PATH/intvenom.py" >> ~/.bashrc or ~/.zshrc
+
+echo "alias intofficial='python3 $INTFRAMEWORK_PATH/intcam.py" >> ~/.bashrc or ~/.zshrc
+
+echo "alias intofficial='python3 $INTFRAMEWORK_PATH/İNTOFFİCİAL.py" >> ~/.bashrc or ~/.zshrc
+
+echo "alias intninja='python3 $INTFRAMEWORK_PATH/intninja.py">> ~/.bashrc or ~/.zshrc
+
+echo "alias intmail='python3 $INTFRAMEWORK_PATH/intmail.py' ">> ~/.bashrc or ~/.zshrc
+
+echo "alias intmeterpreter='python3 $INTFRAMEWORK_PATH/intmeterpreter.py' ">> ~/.bashrc or ~/.zshrc
+
 
 # .bashrc dosyasını yeniden yükleyin.
 source ~/.bashrc
@@ -32,8 +51,9 @@ intmail -h
 
 chmod +x oip
 mkdir -p ~/intmodules
-mv /data/data/com.termux/files/home/intframework/oip ~/intmodules/
-echo 'export PATH=$HOME/intmodules:$PATH' >> ~/.profile
+mv oip ~/intmodules/
+mv ~/intmodules/ $PREFIX/opt/
+echo 'export PATH=$PREFIX/opt/intmodules:$PATH' >> ~/.profile
 source ~/.profile
 oip -h
 cd intframework
