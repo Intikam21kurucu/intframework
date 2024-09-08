2ab5f1cef3ca6f349836dfaf8c05cbc309aa8b74
İNTİKAM21 CYBER <167871296+Intikam21kurucu@users.noreply.github.com>
2024-09-08 08:20:50 +0300

    Update terbuild.sh

--- terbuild.sh
+++ terbuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Bu script, Termux'ta 'intconsole' komutu için bir alias oluşturur ve .bashrc ya da .zshrc dosyasına ekler.
+# Bu script, Termux'ta 'intconsole' komutu için bir alias oluşturur ve ~/.bashrc dosyasına ekler.
 
 chmod +x terbuild.sh
 
@@ -10,23 +10,24 @@ if [ ! -f "$HOME/intframework/.install_done" ]; then
     touch "$HOME/intframework/.install_done"
 fi
 
+# Gerekli Python paketlerini yükleyin
 pip3 install -r requirements.txt
 
 cd ~
 
+# intconsole dosyasına çalıştırma izni ver ve PATH'e ekle
 chmod +x ~/intframework/intconsole
-mv ~/intframework/intconsole $PREFIX/bin
 
+# intframework dizinini $PREFIX/opt/ dizinine taşı
+mv ~/intframework/intconsole $PREFIX/bin/
+mv ~/intframework $PREFIX/opt/
+
+# Zsh kurulumu, isteğe bağlı olarak
 pkg install zsh
 
-# '.bashrc' veya '.zshrc' kontrolü
-if [ -f "~/.bashrc" ]; then
-    SHELLRC="~/.bashrc"
-else
-    SHELLRC="~/.zshrc"
-fi
+# .bashrc dosyasına alias ekleyin
+SHELLRC="$HOME/.bashrc"
 
-# 'intconsole' komutunu tanımlayın ve alias ekleyin.
 echo "alias ll='ls -la'" >> $SHELLRC
 echo "alias intupdate='apt update && apt upgrade -y && rm -rf ~/intframework && git clone https://github.com/Intikam21kurucu/intframework'" >> $SHELLRC
 echo "export INTFRAMEWORK_PATH=$PREFIX/opt/intframework" >> $SHELLRC
@@ -35,8 +36,10 @@ echo "alias intframework=\$INTFRAMEWORK_PATH" >> $SHELLRC
 echo "alias c='clear'" >> $SHELLRC
 echo "alias gs='git status'" >> $SHELLRC
 
-echo 'intconsole() { ORIGINAL_DIR=$(pwd); cd $INTFRAMEWORK_PATH; python3 intconsoleV4.py; cd $ORIGINAL_DIR; [ -n "$BASH" ] && source ~/.bashrc || source ~/.zshrc; }; alias intconsole="intconsole"' >> $SHELLRC
+# intconsole fonksiyonunu ekleyin
+echo 'intconsole() { ORIGINAL_DIR=$(pwd); cd $INTFRAMEWORK_PATH; python3 intconsoleV4.py; cd $ORIGINAL_DIR; source ~/.bashrc; }; alias intconsole="intconsole"' >> $SHELLRC
 
+# Diğer alias'ları ekleyin
 echo "alias introjan='python3 \$INTFRAMEWORK_PATH/introjan.py'" >> $SHELLRC
 echo "alias intvenom='python3 \$INTFRAMEWORK_PATH/intvenom.py'" >> $SHELLRC
 echo "alias intofficial='python3 \$INTFRAMEWORK_PATH/intcam.py'" >> $SHELLRC
@@ -44,7 +47,7 @@ echo "alias intninja='python3 \$INTFRAMEWORK_PATH/intninja.py'" >> $SHELLRC
 echo "alias intmail='python3 \$INTFRAMEWORK_PATH/intmail.py'" >> $SHELLRC
 echo "alias intmeterpreter='python3 \$INTFRAMEWORK_PATH/intmeterpreter.py'" >> $SHELLRC
 
-# .bashrc veya .zshrc dosyasını yeniden yükleyin.
+# ~/.bashrc dosyasını yeniden yükleyin.
 source $SHELLRC
 
 # Ekstra kurulumlar
@@ -56,4 +59,4 @@ echo 'export PATH=$PREFIX/opt/intmodules:$PATH' >> ~/.profile
 source ~/.profile
 oip -h
 
-cd intframework
+cd $PREFIX/opt/intframework
