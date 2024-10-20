b7bcef28d0ce37bccec1619688e7bb7b1d026219
İNTİKAM21 CYBER <167871296+Intikam21kurucu@users.noreply.github.com>
2024-10-20 08:48:09 +0300

    Update http_analyzer.py

--- modules/Auxiliary/http_analyzer.py
+++ modules/Auxiliary/http_analyzer.py
@@ -1,19 +1,16 @@
 import requests
 
-def http_header_analyzer(target_url):
+def http_analyzer(target_url):
+    """[Analyze HTTP headers of the target URL]"""
     try:
         response = requests.get(target_url)
-        print(f"HTTP Başlıkları: {response.headers}")
-        
-        # Güvenlik kontrolleri
-        if "X-Content-Type-Options" not in response.headers:
-            print("Uyarı: X-Content-Type-Options başlığı eksik!")
-        if "X-Frame-Options" not in response.headers:
-            print("Uyarı: X-Frame-Options başlığı eksik!")
-    
+        print(f"HTTP Status Code: {response.status_code}")
+        print("HTTP Headers:")
+        for header, value in response.headers.items():
+            print(f"{header}: {value}")
     except requests.exceptions.RequestException as e:
-        print(f"Hata: {e}")
+        print(f"Error: {e}")
 
-# Kullanım örneği
-target_url = "http://example.com"
-http_header_analyzer(target_url)
+# User input
+target_url = input("\033[91mint4 [Enter the Target URL] > \033[0m")
+http_analyzer(target_url)
