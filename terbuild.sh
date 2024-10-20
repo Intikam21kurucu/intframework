8ff418849a10d10c3e47956dcf7df6db160aff28
İNTİKAM21 CYBER <167871296+Intikam21kurucu@users.noreply.github.com>
2024-10-20 18:51:52 +0300

    Update terbuild.sh

--- terbuild.sh
+++ terbuild.sh
@@ -1,44 +1,51 @@
 #!/bin/bash
 
-# Bu script, Termux'ta 'intconsole' komutu için bir alias oluşturur ve ~/.bashrc dosyasına ekler.
+set -e
 
+# Bu script, Termux'ta 'intconsole' komutu için bir alias oluşturur ve ~/.bashrc dosyasına ekler.
 chmod +x terbuild.sh
 
 # İlk çalıştırma için bir kontrol dosyası oluşturun.
 if [ ! -f "$HOME/intframework/.install_done" ]; then
     python3 "$HOME/intframework/installintconsole.py"
     touch "$HOME/intframework/.install_done"
+    echo "Kurulum tamamlandı."
 fi
 
 # Gerekli Python paketlerini yükleyin
 pip3 install -r requirements.txt
+echo "Gerekli Python paketleri yüklendi."
 
 cd ~
 
 # intconsole dosyasına çalıştırma izni ver ve PATH'e ekle
 chmod +x ~/intframework/intconsole
-
-# intframework dizinini $PREFIX/opt/ dizinine taşı
-chmod +x ~/intframework/intconsole
 mv ~/intframework/intconsole $PREFIX/bin/
 mv ~/intframework $PREFIX/opt/
+echo "intconsole dosyası taşındı."
 
 # Zsh kurulumu, isteğe bağlı olarak
-pkg install zsh
+read -p "Zsh kurmak istiyor musunuz? (e/h): " choice
+if [ "$choice" == "e" ]; then
+    pkg install zsh
+fi
 
 # .bashrc dosyasına alias ekleyin
 SHELLRC="$HOME/.bashrc"
 
+if ! grep -q "export INTFRAMEWORK_PATH" $SHELLRC; then
+    echo "export INTFRAMEWORK_PATH=$PREFIX/opt/intframework" >> $SHELLRC
+fi
+
 echo "alias ll='ls -la'" >> $SHELLRC
 echo "alias intupdate='apt update && apt upgrade -y && rm -rf ~/intframework && git clone https://github.com/Intikam21kurucu/intframework'" >> $SHELLRC
-echo "export INTFRAMEWORK_PATH=$PREFIX/opt/intframework" >> $SHELLRC
 echo "export INTDIR=$PREFIX/opt/" >> $SHELLRC
 echo "alias intframework=\$INTFRAMEWORK_PATH" >> $SHELLRC
 echo "alias c='clear'" >> $SHELLRC
 echo "alias gs='git status'" >> $SHELLRC
 
 # intconsole fonksiyonunu ekleyin
-echo 'intconsole() { ORIGINAL_DIR=$(pwd); cd $INTFRAMEWORK_PATH; python3 intconsoleV4.py; cd $ORIGINAL_DIR; source ~/.bashrc; }; alias intconsole="intconsole"' >> $SHELLRC
+echo 'intconsole() { ORIGINAL_DIR=$(pwd); cd $INTFRAMEWORK_PATH; python3 intconsoleV4.py; cd $ORIGINAL_DIR; }; alias intconsole="intconsole"' >> $SHELLRC
 
 # Diğer alias'ları ekleyin
 echo "alias introjan='python3 \$INTFRAMEWORK_PATH/introjan.py'" >> $SHELLRC
