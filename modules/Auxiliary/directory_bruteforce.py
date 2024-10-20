cb3aed9dd2d039e2f06805d254429e4d837b5844
İNTİKAM21 CYBER <167871296+Intikam21kurucu@users.noreply.github.com>
2024-10-20 08:46:54 +0300

    Update directory_bruteforce.py

--- modules/Auxiliary/directory_bruteforce.py
+++ modules/Auxiliary/directory_bruteforce.py
@@ -1,6 +1,7 @@
 import requests
 
 def directory_bruteforce(target_url, wordlist):
+    """[Bruteforce directories on a target website]"""
     found_dirs = []
 
     with open(wordlist, 'r') as f:
@@ -10,17 +11,17 @@ def directory_bruteforce(target_url, wordlist):
         dir_path = directory.strip()
         url = target_url + "/" + dir_path
         response = requests.get(url)
-
+        
         if response.status_code == 200:
-            print(f"Bulundu: {url}")
+            print(f"Found Directory: {url}")
             found_dirs.append(url)
-    
+
     if not found_dirs:
-        print("Dizin bulunamadı.")
+        print("No directories found.")
     
     return found_dirs
 
-# Kullanım örneği
-target_url = "http://example.com"
-wordlist = "common_directories.txt"  # Dizin listesi
-directory_bruteforce(target_url, wordlist)
+# User input
+target_url = input("\033[91mint4 [Enter the Target URL] > \033[0m")
+wordlist_path = input("\033[91mint4 [Enter the Path to the Wordlist] > \033[0m")
+directory_bruteforce(target_url, wordlist_path)
