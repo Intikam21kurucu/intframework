d4ec7b689428591e0ce7c08d67e19b2785adbf43
intSpLoiT Framework Project <167871296+intSpLoiT@users.noreply.github.com>
2025-03-01 08:55:45 +0300

    Update gpstracker.py

--- modules/post/android/all/gpstracker.py
+++ modules/post/android/all/gpstracker.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: @bobsecq
+# title: Android GPS tracker
+# test: good
 import requests
 from jnius import autoclass, cast
 from plyer import gps
