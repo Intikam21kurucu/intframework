3da2541965dee3dffcc91fdcfc2a24182304ca7b
intSpLoiT Framework Project <167871296+intSpLoiT@users.noreply.github.com>
2025-05-10 19:23:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -111,5 +111,5 @@ def menu():
     return render_template('menu.html')
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 443))
     app.run(host="0.0.0.0", port=port, debug=True)
