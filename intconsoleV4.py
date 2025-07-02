67d6a8397ac10e8d88e0b52d5856f5352cf24904
intSpLoiT Framework Project <167871296+intSpLoiT@users.noreply.github.com>
2025-07-02 09:10:51 +0300

    Add files via upload
    Signed-off-by: intSpLoiT Framework Project <167871296+intSpLoiT@users.noreply.github.com>

--- intconsoleV4.py
+++ intconsoleV4.py
@@ -1657,8 +1657,9 @@ print(" ")
 global running_pid
 running_pid = None        
 from prompt_toolkit import PromptSession
+history_path = os.path.expanduser("~/.intframework_history.txt")
 session = PromptSession(
-    history=FileHistory("~/.intframework_history.txt"),
+    history=FileHistory(history_path),
     lexer=CommandColorLexer(),
     style=style
 )
